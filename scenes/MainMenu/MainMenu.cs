4da8a13105b086eb086355fa5c287065f82a8ece
Pr0pyh <andrej.drmanovic@gmail.com>
2025-05-10 23:14:32 +0200

    ispravka

--- scenes/MainMenu/MainMenu.cs
+++ scenes/MainMenu/MainMenu.cs
@@ -3,13 +3,12 @@ using System;
 
 public partial class MainMenu : Control
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+	public void _on_button_pressed()
 	{
+		GetTree().ChangeSceneToFile("res://scenes/World/World.tscn");
 	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	public void _on_button_2_pressed()
 	{
+		GetTree().Quit();
 	}
 }
